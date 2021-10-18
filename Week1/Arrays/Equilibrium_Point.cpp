3b86571449bc6b3490557312325455457988344d
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-10-18 16:40:38 +0530

    code added

---
+++ Week1/Arrays/Equilibrium_Point.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        int i;
+        long long asum = 0;
+        for(i=0;i<n;i++){
+            asum+=a[i];
+        }
+        long long lsum=0;
+        for(i=0;i<n;i++){
+            asum-=a[i];
+            if(asum == lsum){
+                return i+1;
+            }
+            lsum+=a[i];
+        }
+        
+        return -1;
+        
+    }
+
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
