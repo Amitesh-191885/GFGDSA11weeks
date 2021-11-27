b2ba2b7a0e07b4c5990e4336c7fedbf20d414221
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-11-27 09:43:30 +0530

    code added

---
+++ Week1/Arrays/Find_Immediate_Smaller_X.cpp
@@ -0,0 +1,67 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // User function to complete
+    // arr[]: input array
+    // n: size of array
+    // k: find element smaller and closest to it. 
+    int immediateSmaller(int arr[], int n, int x)
+    {
+        // your code here
+        int X = *max_element(arr, arr+n);
+        sort(arr, arr+n);
+        if(x > X){
+            return X;
+        }
+        else{
+        for(int i=0; i < n-1; i++){
+            if(i==0 && arr[i] == x){
+                return -1;
+            }
+            else if(arr[i]<x && arr[i+1]>=x){
+                return arr[i];
+            }
+        }
+        
+        }
+        return -1;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    
+            int n;
+        	cin >> n;
+        	
+        	int arr[n];
+        	
+        	for(int i = 0;i<n;i++){
+        	    cin >> arr[i];
+        	}
+        	
+        	int x;
+        	cin >> x;
+        	Solution ob;
+        	cout << ob.immediateSmaller(arr, n, x) << endl;
+	}
+	
+	
+	return 0;
+}
+  // } Driver Code Ends
