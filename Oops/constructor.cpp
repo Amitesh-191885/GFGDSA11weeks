6d1191e0d38d1eecd3bacae52a1147d6deaa73cc
Amitesh Ranjan <amiteshranjan600@gmail.com>
2022-01-17 17:45:16 +0530

    code added

--- Oops/constructor.cpp
+++ Oops/constructor.cpp
@@ -15,20 +15,30 @@ class Hero{
         this->name = name;
         cout<<"This is parameterised constructor."<<endl;
     }
+
+    void getHero(){
+        cout<<"Hero name: "<<name<<endl;
+        cout<<"Hero age: "<<age<<endl;
+        cout<<endl;
+    }
 };
 
 int main(){
     //static object creation with default constructor.
     Hero raju;
+    raju.getHero();
 
     //static object creation with parameterised constructor.
     Hero Raju(19,"Raju");
+    Raju.getHero();
 
     //Dynamic object creation with default constructor.
     Hero *Person = new Hero;
+    Person->getHero();
 
     //Dynamic object creation with parametrised constructor.
 
     Hero *rani = new Hero(18,"Rani");
+    rani->getHero();
     return 0;
 }
